1c11a71c7833f418b7c3a1db1a5a09f2918502bd
Your Name <you@example.com>
2023-09-06 01:11:22 -0400

    skills chips, tech added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/home.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { SkillsComponent } from './shared/skills/skills.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    SkillsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,6 +29,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/skills/skills.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+})
+export class SkillsComponent {}
